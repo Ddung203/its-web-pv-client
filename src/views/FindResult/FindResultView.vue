e6838106c0aaa298e431844cdbf71be821a49fd5
ddung203 <duongvandung2k3@gmail.com>
2024-09-03 01:03:35 +0700

    FindResultView.vue

--- src/views/FindResult/FindResultView.vue
+++ src/views/FindResult/FindResultView.vue
@@ -1,12 +1,142 @@
 <script setup>
   import { ref } from "vue";
   import Header from "@/components/Header/Header.vue";
+  import Footer from "@/components/Footer/Footer.vue";
+  import { useToast } from "primevue/usetoast";
+  import HTTP from "../../helper/axiosInstance";
+  import showNotification from "../../utils/showNotification";
+
+  const studentCode = ref("");
+  const isPassed = ref("1");
+  const visible = ref(false);
+
+  const toast = useToast();
+
+  const studentPassed = ref(false);
+
+  const onSubmitHandle = async () => {
+    studentPassed.value = false;
+    if (studentCode.value.trim() == "") {
+      showNotification(
+        toast,
+        "error",
+        "Thông báo",
+        "Mã sinh viên không được để trống!",
+        1500
+      );
+      return;
+    }
+
+    try {
+      const response = await HTTP.get(
+        `/user/find-result?studentCode=${studentCode.value.trim()}&isPassed=${
+          isPassed.value
+        }`
+      );
+
+      if (response.success) {
+        visible.value = true;
+        studentPassed.value = true;
+      }
+    } catch (e) {
+      visible.value = true;
+      studentPassed.value = false;
+    }
+  };
 </script>
 
 <template>
   <Header></Header>
-  <h1>Tra cứu kết quả</h1>
-  <p>Comming soon!</p>
+
+  <div class="top-content">
+    <p class="pt-11 text-4xl text-center font-bold text-[#000] uppercase">
+      Tra cứu kết quả chính thức
+    </p>
+
+    <div class="flex items-center justify-center gap-[30px] mt-5">
+      <div class="h-1 bg-[#000] w-28"></div>
+      <div class="text-[#000]"><i class="text-4xl pi pi-star-fill"></i></div>
+      <div class="h-1 bg-[#000] w-28"></div>
+    </div>
+
+    <p class="pt-7 px-[20px] text-xl text-center font-bold text-[#000]">
+      Nhập thông tin của bạn (mã sinh viên) để xem kết quả
+    </p>
+  </div>
+
+  <!--  -->
+  <form
+    @submit.prevent="onSubmitHandle"
+    autocomplete="off"
+  >
+    <div
+      class="grid grid-cols-1 lg:grid-cols-2 grid-rows-1 gap-12 mt-6 pt-4 lg:pt-0 px-[20px] lg:px-[160px]"
+    >
+      <div class="flex flex-col items-center justify-center w-full gap-4">
+        <FloatLabel class="w-full">
+          <InputText
+            class="w-full"
+            id="studentCode"
+            v-model="studentCode"
+          />
+          <label for="studentCode">Mã sinh viên</label>
+        </FloatLabel>
+
+        <Button
+          class="w-full"
+          label="Tra cứu"
+          @click="onSubmitHandle"
+        ></Button>
+      </div>
+      <div class="hidden overflow-hidden rounded-md lg:block">
+        <img
+          class="block max-w-full"
+          src="../../public/assets/imgs/2v2.png"
+          alt="It's you and me"
+        />
+      </div>
+    </div>
+  </form>
+
+  <!-- Dialog -->
+
+  <Dialog
+    v-model:visible="visible"
+    modal
+    header=" "
+    :style="{ width: '25rem' }"
+  >
+    <div
+      class="passed"
+      v-if="studentPassed"
+    >
+      <div class="flex items-center justify-center text-green-600">
+        <i class="text-6xl pi pi-check-circle"></i>
+      </div>
+
+      <p class="pt-4 pb-8 text-xl text-center text-green-600">
+        Chúc mừng bạn đã trở thành cộng tác viên của CLB Hỗ trợ kỹ thuật IT
+        Supporter HaUI. <br /><br />Thông tin chi tiết vui lòng kiểm tra email
+        của bạn 😘
+      </p>
+    </div>
+
+    <div
+      class="no-passed"
+      v-if="!studentPassed"
+    >
+      <div class="flex items-center justify-center text-red-500">
+        <i class="text-6xl pi pi-times-circle"></i>
+      </div>
+      <p class="pt-4 pb-6 text-xl text-center text-red-500">
+        Không tìm thấy mã sinh viên hoặc bạn đã không đủ điều kiện trúng tuyển!
+      </p>
+    </div>
+  </Dialog>
+
+  <div class="absolute bottom-0 left-0 w-full">
+    <Footer></Footer>
+  </div>
 </template>
 
 <style scoped>
